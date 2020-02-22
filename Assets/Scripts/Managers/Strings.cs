cdf3aeee0bd0f21f78d8612f7c1f8328ff98a240
konan01 <crazy_vrn@mail.ru>
2020-02-22 15:07:57 +0300

    add global strings

---
+++ Assets/Scripts/Managers/Strings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings 
+{
+    public static string newGame;
+    public static string login;
+    public static string newRoom;
+    public static string roomPassword;
+    public static string settings;
+    public static string score ="Очки:";
+}
